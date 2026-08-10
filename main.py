f7defdd0482f0ce1e493cb9f6fa462e52af80f2f
DEV-LucaShop <ngngxikla@gmail.com>
2026-08-10 12:43:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ REPO_NAME = "Bottt"
 FILE_PATH = "hwid.json"
 
 # 📌 ค่าลิงก์เชิญเข้าเซิร์ฟเวอร์หลักของคุณ (สามารถเปลี่ยนลิงก์ตรงนี้หรือใส่ใน Environment Variables บน Render ได้)
-MAIN_SERVER_INVITE = os.getenv("MAIN_SERVER_INVITE", "https://discord.gg/your-main-server")
+MAIN_SERVER_INVITE = os.getenv("MAIN_SERVER_INVITE", "https://discord.gg/whahzWC4NU")
 
 # 📌 ดึงค่าผ่าน Environment Variables บน Render
 ALLOWED_CHANNEL_ID = int(os.getenv("ALLOWED_CHANNEL_ID", "1536234943431053333"))
 ADMIN_ROLE_ID = int(os.getenv("ADMIN_ROLE_ID", "1533642657413464247"))
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "")  
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://ptb.discord.com/api/webhooks/1536237329847554100/LtnZDy7eey-NHJhO-PXQ_6erUqCKG4K60PEbs4uB4CavLAr4iV6pbwfcHUyR-nnEQ97O")  
 # ==========================================================
 
 intents = discord.Intents.default()
